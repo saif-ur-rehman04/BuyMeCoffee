e3b4042f2d0b74c128b4895215379ff520720073
Saif ur Rehman <saif@pryvatenow.com>
2023-05-25 18:00:33 +0500

    initial commi

---
+++ scripts/buy-coffee.js
@@ -0,0 +1,72 @@
+const hre = require("hardhat");
+
+// Returns the Ether balance of a given address.
+async function getBalance(address) {
+  const balanceBigInt = await hre.ethers.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+// Logs the Ether balances for a list of addresses.
+async function printBalances(addresses) {
+  let idx = 0;
+  for (const address of addresses) {
+    console.log(`Address ${idx} balance: `, await getBalance(address));
+    idx ++;
+  }
+}
+
+// Logs the memos stored on-chain from coffee purchases.
+async function printMemos(memos) {
+  for (const memo of memos) {
+    const timestamp = memo.timestamp;
+    const tipper = memo.name;
+    const tipperAddress = memo.from;
+    const message = memo.message;
+    console.log(`At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`);
+  }
+}
+
+async function main() {
+  // Get the example accounts we'll be working with.
+  const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
+
+  // We get the contract to deploy.
+  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+  const buyMeACoffee = await BuyMeACoffee.deploy();
+
+  // Deploy the contract.
+  await buyMeACoffee.deployed();
+  console.log("BuyMeACoffee deployed to:", buyMeACoffee.address);
+
+  // Check balances before the coffee purchase.
+  const addresses = [owner.address, tipper.address, buyMeACoffee.address];
+  console.log("== start ==");
+  await printBalances(addresses);
+
+//buy cofee
+const tip = {value: hre.ethers.utils.parseEther("1")};
+ await buyMeACoffee.connect(tipper).buyCoffee("honey","nice cofee", tip);
+ await buyMeACoffee.connect(tipper).buyCoffee("maan","not bad",tip);
+ await buyMeACoffee.connect(tipper).buyCoffee("aliza","awesome",tip);
+//check ablance after buying cofee
+console.log("== Checking balance again ==");
+await printBalances(addresses);
+
+//withdraw money
+await buyMeACoffee.connect(owner).withdrawTips();
+
+//check balance after withdrawing
+console.log("== Checking balance after withdrawal ==");
+await printBalances(addresses);
+
+// Read memos
+const meme =await buyMeACoffee.getMemos();
+await printMemos(meme);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
